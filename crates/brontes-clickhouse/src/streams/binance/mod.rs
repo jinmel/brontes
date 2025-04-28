9950327f214a7a3d5fa30214ee8b90fd952467b0
Jinsuk Park <totorovirus@gmail.com>
2025-04-28 12:15:58 +0900

    Define types for exchange WebSocketStream handles

--- crates/brontes-clickhouse/src/streams/binance/mod.rs
+++ crates/brontes-clickhouse/src/streams/binance/mod.rs
@@ -1,73 +1,56 @@
-use crate::streams::{StreamConnection, DataStream, Exchange};
+use crate::streams::{Exchange, StreamConnectionError};
 use crate::models::{NormalizedTrade, NormalizedQuote};
-use futures::stream::{Stream, BoxStream};
+use futures::stream::{BoxStream};
 use async_trait::async_trait;
 use eyre::Result;
-use std::error::Error as StdError;
-use std::io;
+use tokio_tungstenite::{connect_async, WebSocketStream, MaybeTlsStream};
+use tokio::net::TcpStream;
+use tokio_tungstenite::tungstenite::http::Uri;
 
-pub struct BinanceStream;
-
-#[async_trait]
-impl StreamConnection for BinanceStream {
-    type Error = io::Error;
-
-    async fn connect(&mut self) -> Result<(), Self::Error> {
-        // Implementation details to be added
-        Ok(())
-    }
-
-    async fn disconnect(&mut self) -> Result<(), Self::Error> {
-        // Implementation details to be added
-        Ok(())
-    }
+async fn connect_to_binance_quotes() -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, StreamConnectionError> {
+    let url = "wss://fstream.binance.com/ws/btc@arr".parse::<Uri>()
+        .map_err(|e| StreamConnectionError::ConnectionFailed(e.to_string()))?;
+    let (ws_stream, _) = connect_async(url).await
+        .map_err(|e| StreamConnectionError::ConnectionFailed(e.to_string()))?;
+    Ok(ws_stream)
 }
 
-pub struct BinanceTradeStream {
-    connection: BinanceStream,
-}
-
-pub struct BinanceQuoteStream {
-    connection: BinanceStream,
-}
-
-#[async_trait]
-impl DataStream<NormalizedTrade, BinanceStream> for BinanceTradeStream {
-    async fn get(&self) -> Result<BoxStream<'_, Result<NormalizedTrade, <BinanceStream as StreamConnection>::Error>>, <BinanceStream as StreamConnection>::Error> {
-        // Implementation details to be added
-        todo!()
-    }
-}
-
-#[async_trait]
-impl DataStream<NormalizedQuote, BinanceStream> for BinanceQuoteStream {
-    async fn get(&self) -> Result<BoxStream<'_, Result<NormalizedQuote, <BinanceStream as StreamConnection>::Error>>, <BinanceStream as StreamConnection>::Error> {
-        // Implementation details to be added
-        todo!()
-    }
+async fn connect_to_binance_trades() -> Result<WebSocketStream<MaybeTlsStream<TcpStream>>, StreamConnectionError> {
+    let url = "wss://fstream.binance.com/ws/btc@arr".parse::<Uri>()
+        .map_err(|e| StreamConnectionError::ConnectionFailed(e.to_string()))?;
+    let (ws_stream, _) = connect_async(url).await
+        .map_err(|e| StreamConnectionError::ConnectionFailed(e.to_string()))?;
+    Ok(ws_stream)
 }
 
 pub struct Binance {
-    normalized_trades: BinanceTradeStream,
-    normalized_quotes: BinanceQuoteStream,
-}
-
-#[async_trait]
-impl Exchange<BinanceStream> for Binance {
-    async fn normalized_trades(&self) -> Result<BoxStream<'_, Result<NormalizedTrade, <BinanceStream as StreamConnection>::Error>>, <BinanceStream as StreamConnection>::Error> {
-        self.normalized_trades.get().await
-    }
-
-    async fn normalized_quotes(&self) -> Result<BoxStream<'_, Result<NormalizedQuote, <BinanceStream as StreamConnection>::Error>>, <BinanceStream as StreamConnection>::Error> {
-        self.normalized_quotes.get().await
-    }
+    normalized_trades: WebSocketStream<MaybeTlsStream<TcpStream>>,
+    normalized_quotes: WebSocketStream<MaybeTlsStream<TcpStream>>,
 }
 
 impl Binance {
-    pub fn new(trades_stream: BinanceTradeStream, quotes_stream: BinanceQuoteStream) -> Self {
-        Self {
-            normalized_trades: trades_stream,
-            normalized_quotes: quotes_stream,
-        }
+    pub async fn new() -> Result<Self, StreamConnectionError> {
+        let normalized_trades = connect_to_binance_trades().await?;
+        let normalized_quotes = connect_to_binance_quotes().await?;
+
+        Ok(Self {
+            normalized_trades,
+            normalized_quotes,
+        })
+    }
+}
+
+#[async_trait]
+impl Exchange<MaybeTlsStream<TcpStream>> for Binance {
+    async fn normalized_trades(&self) -> Result<BoxStream<'_, Result<NormalizedTrade, StreamConnectionError>>, StreamConnectionError> {
+        // TODO: Implement normalized trades stream
+        // TODO: Parse Binance response into NormalizedTrade
+        unimplemented!("Implement normalized trades stream")
+    }
+
+    async fn normalized_quotes(&self) -> Result<BoxStream<'_, Result<NormalizedQuote, StreamConnectionError>>, StreamConnectionError> {
+        // TODO: Implement normalized quotes stream
+        // TODO: Parse Binance response into NormalizedQuote
+        unimplemented!("Implement normalized quotes stream")
     }
 }
--- crates/brontes-clickhouse/src/streams/mod.rs
+++ crates/brontes-clickhouse/src/streams/mod.rs
@@ -1,25 +1,46 @@
 pub mod binance;
 
 use eyre::Result;
+use tokio_tungstenite::WebSocketStream;
 use crate::models::{NormalizedTrade, NormalizedQuote};
 use futures::stream::Stream;
 use async_trait::async_trait;
+use std::fmt;
+use tokio::io::{AsyncRead, AsyncWrite};
+
+#[derive(Debug)]
+pub enum StreamConnectionError {
+    ConnectionFailed(String),
+    DisconnectionFailed(String),
+    StreamError(String),
+    StreamNotConnected(String),
+}
+
+impl std::error::Error for StreamConnectionError {}
+
+impl fmt::Display for StreamConnectionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            StreamConnectionError::ConnectionFailed(msg) => write!(f, "Connection failed: {}", msg),
+            StreamConnectionError::DisconnectionFailed(msg) => write!(f, "Disconnection failed: {}", msg),
+            StreamConnectionError::StreamError(msg) => write!(f, "Stream error: {}", msg),
+            StreamConnectionError::StreamNotConnected(msg) => write!(f, "Stream not connected: {}", msg),
+        }
+    }
+}
 
 #[async_trait]
-pub trait StreamConnection {
+pub trait StreamConnection<T> where T: AsyncRead + AsyncWrite + Unpin
+{
     type Error: std::error::Error + Send + Sync + 'static; 
 
-    async fn connect(&mut self) -> Result<(), Self::Error>;
+    async fn connect<'a>(&'a mut self) -> Result<&'a WebSocketStream<T>, Self::Error>;
     async fn disconnect(&mut self) -> Result<(), Self::Error>;
+    async fn get_stream<'a>(&'a self) -> Result<&'a WebSocketStream<T>, Self::Error>;
 }
 
 #[async_trait]
-pub trait DataStream<T, C> where C: StreamConnection {
-    async fn get(&self) -> Result<impl Stream<Item = Result<T, C::Error>> + Send + '_, C::Error>;
-}
-
-#[async_trait]
-pub trait Exchange<C> where C: StreamConnection {
-    async fn normalized_trades(&self) -> Result<impl Stream<Item = Result<NormalizedTrade, C::Error>> + Send + '_, C::Error>;
-    async fn normalized_quotes(&self) -> Result<impl Stream<Item = Result<NormalizedQuote, C::Error>> + Send + '_, C::Error>;
+pub trait Exchange<T> where T: AsyncRead + AsyncWrite + Unpin {
+    async fn normalized_trades(&self) -> Result<impl Stream<Item = Result<NormalizedTrade, StreamConnectionError>> + Send + '_, StreamConnectionError>;
+    async fn normalized_quotes(&self) -> Result<impl Stream<Item = Result<NormalizedQuote, StreamConnectionError>> + Send + '_, StreamConnectionError>;
 }
