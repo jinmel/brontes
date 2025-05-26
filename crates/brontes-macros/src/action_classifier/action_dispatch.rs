8852996c4d4f7508e4e296c3d3ceef89af595ec4
marsclimber0109 <marsclimber0109@gmail.com>
2025-05-26 18:11:54 +0900

    chore: rm get action tracing

--- crates/brontes-macros/src/action_classifier/action_dispatch.rs
+++ crates/brontes-macros/src/action_classifier/action_dispatch.rs
@@ -120,7 +120,6 @@ fn expand_match_dispatch(
                     db_tx
                 ).map(|res| {
                     let action = res.get_action();
-                    ::tracing::info!("get action succeeded result: {:?}", action.clone());
                     Some((res, action))
                  }).unwrap_or_else(|e| {
                         ::tracing::warn!(error=%e,
