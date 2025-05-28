bf4f483e2029afe4e70f2e427fec2b0d10790b78
Jinsuk Park <totorovirus@gmail.com>
2025-05-28 18:42:24 +0900

    trace failed

--- crates/brontes-core/src/decoding/parser.rs
+++ crates/brontes-core/src/decoding/parser.rs
@@ -262,6 +262,10 @@ impl<T: TracingProvider, DB: LibmdbxReader + DBWriter> TraceParser<T, DB> {
             .replay_block_transactions(BlockId::Number(BlockNumberOrTag::Number(block_num)))
             .await;
 
+        if let Err(e) = merged_trace {
+            error!(%block_num, "failed to trace block: {:?}", e);
+        }
+
         let mut stats = BlockStats::new(block_num, None);
         let trace = match merged_trace {
             Ok(Some(t)) => Some(t),
