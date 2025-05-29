5a00b4417e95ca1380edbd7cac026e1384eba7c1
Jinsuk Park <totorovirus@gmail.com>
2025-05-29 16:46:01 +0900

    Add timeboost.bids table

---
+++ crates/brontes-database/brontes-db/src/clickhouse/tables/bids.sql
@@ -0,0 +1,10 @@
+CREATE TABLE timeboost.bids (
+    chain_id UInt64,
+    bidder String,
+    express_lane_controller String,
+    auction_contract_address String,
+    round UInt64,
+    amount String,
+    signature String
+) ENGINE = MergeTree()
+ORDER BY round
