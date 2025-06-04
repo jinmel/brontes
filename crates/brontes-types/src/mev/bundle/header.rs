1de07ed7291f937361f76d222aed7ef8da70030f
Jinsuk Park <totorovirus@gmail.com>
2025-06-05 02:36:54 +0900

    add timeboosted column to bundle_header table

--- crates/brontes-types/src/mev/bundle/header.rs
+++ crates/brontes-types/src/mev/bundle/header.rs
@@ -193,7 +193,7 @@ impl Serialize for BundleHeader {
             })
             .collect_vec();
         ser_struct.serialize_field("balance_deltas.token_deltas", &balance_deltas_token_deltas)?;
-
+        ser_struct.serialize_field("timeboosted", &self.timeboosted)?;
         ser_struct.end()
     }
 }
@@ -214,5 +214,6 @@ impl DbRow for BundleHeader {
         "balance_deltas.address",
         "balance_deltas.name",
         "balance_deltas.token_deltas",
+        "timeboosted",
     ];
 }
