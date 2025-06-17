ed30435751bcbb2e894408a4486aa0dd93e18546
Jinsuk Park <totorovirus@gmail.com>
2025-06-17 22:01:47 +0900

    abnormal profit threshold for atomic arbitrage reduced from 100 USD to 30 USD

--- crates/brontes-inspect/src/mev_inspectors/atomic_arb.rs
+++ crates/brontes-inspect/src/mev_inspectors/atomic_arb.rs
@@ -274,7 +274,7 @@ impl<DB: LibmdbxReader> AtomicArbInspector<'_, DB> {
         );
 
         self.utils.get_profit_metrics().inspect(|m| {
-            if profit_usd.abs() > 100.0 {
+            if profit_usd.abs() > 30.0 {
                 tracing::warn!(?header.tx_hash, ?profit_usd, "abnormal profit for arb type: {}", possible_arb_type);
                 m.publish_abnormal_profit(MevType::AtomicArb, &protocols, profit_usd);
             }
