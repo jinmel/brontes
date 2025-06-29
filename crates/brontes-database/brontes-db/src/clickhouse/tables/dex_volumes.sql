a7aa57e5a9b962195e8833b2d5d31cf96e9d6960
Jinsuk Park <totorovirus@gmail.com>
2025-06-30 02:55:00 +0900

    make volume_usd and recipient nullable in dex_volumes table

--- crates/brontes-database/brontes-db/src/clickhouse/tables/dex_volumes.sql
+++ crates/brontes-database/brontes-db/src/clickhouse/tables/dex_volumes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS dex.dex_volumes (
     `period` DateTime64(3, 'UTC'),
     `project` String,
-    `volume_usd` Float64,
-    `recipient` UInt64
+    `volume_usd` Nullable(Float64) DEFAULT 0,
+    `recipient` Nullable(UInt64) DEFAULT 0
 ) ENGINE = ReplacingMergeTree()
 ORDER BY (`period`, `project`)
