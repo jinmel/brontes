f20d81273ebf09970f7ad32a0c737dec06cf42bf
jinmel <totorovirus@gmail.com>
2025-08-21 19:47:17 +0900

    style(run): update arg defaults and formatting (#80)

--- run.sh
+++ run.sh
@@ -1,33 +1,27 @@
 #!/bin/sh
 
-
-RUST_LOG="brontes=warn,brontes_inspect::mev_inspectors::cex_dex::markout=info"
-
-cargo run --release --features arbitrum -- \
+RUST_LOG="brontes=warn,brontes_inspect::mev_inspectors::cex_dex::markout=info" cargo run --features arbitrum -- \
 run \
 --with-metrics \
---behind-tip 50 \
-# VWAP window (tight, post-first)
+--behind-tip 100 \
 --initial-pre 0.0005 \
 --initial-post 0.004 \
 --max-vwap-pre 1.0 \
 --max-vwap-post 1.0 \
 --vwap-scaling-diff 0.02 \
 --vwap-time-step 0.0002 \
---weights-vwap true \
---weights-pre-vwap -0.00007 \
---weights-post-vwap -0.000017 \
-# Optimistic window (tight, post-first)
+--weights-vwap \
+--weights-pre-vwap=-0.00007 \
+--weights-post-vwap=-0.000017 \
 --initial-op-pre 0.0005 \
 --initial-op-post 0.004 \
 --max-op-pre 0.08 \
 --max-op-post 0.16 \
 --optimistic-scaling-diff 0.02 \
 --optimistic-time-step 0.0015 \
---weights-op true \
---weights-pre-op -0.00007 \
---weights-post-op -0.000017 \
-# Misc
+--weights-op \
+--weights-pre-op=-0.00007 \
+--weights-post-op=-0.000017 \
 --quote-offset 0.0 \
 --cex-dex-min-profit-usd 0.5 \
---cex-dex-known-min-profit-usd -1.0
+--cex-dex-known-min-profit-usd=-1.0
